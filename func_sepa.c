6a12a3a78da3cd01bc477dbf0fc320a169c267b2
Ousmane Konate <konatealim@gmail.com>
2022-08-10 11:37:28 +0200

    add

--- func_sepa.c
+++ func_sepa.c
@@ -8,33 +8,32 @@
  */
 int func_separator(char *comd)
 {
-	char *tok, *sepa[] = {";", "||", "&&"};
+	char **tok, *sepa[] = {";", "||", "&&"};
 	int i, j = 0, h = 0;
 
 	for (i = 0; i < 3; i++)
 	{
 		if (_strstr(comd, sepa[i]))
 		{
-			tok = _strtoki(comd, sepa[i]);
-			while (tok)
+			tok = _strtok(comd, sepa[i]);
+			while (tok[0])
 			{
 				if (i == 0)
-					execute_line(tok, 1, environ, h, &h);
+					execute_line(tok[0], 1, environ, h, &h);
 				else if (i == 1)
 				{
 					/*Execute_line return 0 en cas de success
 					  D'où en cas de succès on arrête l'exécution*/
-					if (execute_line(tok, 1, environ, h, &h) == 0)
+					if (execute_line(tok[0], 1, environ, h, &h) == 0)
 						return (1);
 				}
 				else
 				{
 					/*En cas d'erreur (execute_line return != 0) on arrête l'exécution*/
-					if(execute_line(tok, 1, environ, h, &h))
+					if(execute_line(tok[0], 1, environ, h, &h))
 						return (1);
 				}
-
-				tok = _strtoki(NULL, sepa[i]);
+				tok++;
 				j++;
 			}
 			return (1);
