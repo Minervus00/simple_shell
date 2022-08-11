8838d1690cee28719b1b2c7001122ceacac52f43
Ousmane Konate <konatealim@gmail.com>
2022-08-11 17:06:06 +0200

    add

--- executeline.c
+++ executeline.c
@@ -41,7 +41,8 @@ int execute_line(char **av, char *comd, int count,
 				if (access(full_path, X_OK) == 0)
 					if (execve(full_path, argv, env) == -1)
                     {
-                        *exit_st = WEXITSTATUS(status);
+                        if if(WIFEXITED(status))
+                            *exit_st = WEXITSTATUS(status);
                     }
 			}
 			_error(av, argv[0], count, &exit_st);
