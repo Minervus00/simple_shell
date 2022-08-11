65f83f6b2a2731078bf7a326d9ba11e80e4866e6
Ousmane Konate <konatealim@gmail.com>
2022-08-11 18:17:28 +0200

    add

--- executeline.c
+++ executeline.c
@@ -52,9 +52,9 @@ int execute_line(char **av, char *comd, int count,
 			wait(&status);
 			free_loop(argv);
 			*exit_st = WEXITSTATUS(status);
-            if (*exit_st == 2)
-                exit(2);
 			if (*exit_st != 0)
+                if (*exit_st == 2)
+                    exit(2);
 				return (-1);
             
 		}
